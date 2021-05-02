82f063ea4cd80152caa4d1996fae69e29f91fd55
Batmanova <62687203+Batmanova@users.noreply.github.com>
2021-05-02 18:40:22 +0300

    add testing client

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+HOST = 'localhost'
+PORT = 8090
+
+requests = ['GET /pwd/', 'GET /ls/', 'GET /mkdir/new', 'GET /rmdir/new', 'GET /delete/picture.png', 'GET /rename/index.html/index.html', 'GET /receive/index.html', 'GET /stop/']
+sock = socket.socket()
+try:
+    sock.connect((HOST, PORT))
+except Exception as e:
+    print(e)
+for request in requests:
+    sock.send(request.encode())
+    try:
+        response = sock.recv(1024).decode()
+    except Exception as e:
+        print(request)
+        print(e)
+    if response == 'error':
+        print('Error in ' + request)
+sock.close()
