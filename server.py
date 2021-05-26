87ce6a27dd030238c559c3b17460b6e84e6fc055
Batmanova <62687203+Batmanova@users.noreply.github.com>
2021-05-26 16:58:07 +0300

    add comments

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import socket
 import os
 import threading
 
+#для работы сервера необходимо его запустить и по заданной отправлять get-запросы. клиент из другого файла просто отправляет запросы, и ничего не выводит в результате
+
+#функция удаления
 def rmdir(path):
     if os.listdir(path) == []:
         try:
@@ -18,19 +21,22 @@ def rmdir(path):
         except:
             return 'error'
 
-
+#запуск отдельного потока для пользователя
 def process(req):
     global users
     user = users[0]
     homedir = '/home/alisa/PycharmProjects/ftp/users/'
+    #запрос http://127.0.0.1:8080/pwd/, возвращает рабочую директорию
     if req.startswith('GET /pwd/'):
         with open('/home/alisa/PycharmProjects/ftp/log.txt', 'w') as logs:
             logs.write('pwd ' + user)
         return os.getcwd()
+    #запрос http://127.0.0.1:8080/ls/, возвращает список файлов в рабочей директории
     elif req.startswith('GET /ls/'):
         with open('/home/alisa/PycharmProjects/ftp/log.txt', 'w') as logs:
             logs.write('ls ' + user)
         return '; '.join(os.listdir())
+    #запрос http://127.0.0.1:8080/mkdir/name_of_dir/, создает папку с заданным именем
     elif req.startswith('GET /mkdir/'):
         name = req.split()[1][7:]
         try:
@@ -40,6 +46,7 @@ def process(req):
             return 'created'
         except OSError:
             return 'error'
+    #запрос http://127.0.0.1:8080/rmdir/name_of_dir/, удаляет заданную папку
     elif req.startswith('GET /rmdir/'):
         name = req.split()[1][7:]
         try:
@@ -50,6 +57,7 @@ def process(req):
             return resp
         except OSError:
             return 'error'
+    #запрос http://127.0.0.1:8080/delete/name_of_file/, удаляет заданный файл
     elif req.startswith('GET /delete/'):
         name = req.split()[1][8:]
         try:
@@ -60,6 +68,7 @@ def process(req):
         except OSError as e:
             print(e)
             return 'error'
+    #запрос http://127.0.0.1:8080/rename/previous_name/new_name/, переименовывает файл
     elif req.startswith('GET /rename/'):
         data = req.split()[1][7:]
         prev = data.split('/')[1].replace('\\', '/')
@@ -72,6 +81,7 @@ def process(req):
         except OSError as e:
             print(e)
             return 'error'
+   #запрос http://127.0.0.1:8080/receive/name_of_file/, отправляет файл
     elif req.startswith('GET /receive/'):
         data = req.split()[1][9:]
         try:
@@ -87,11 +97,13 @@ def process(req):
         except OSError as e:
             print(e)
             return 'error'
+    #запрос http://127.0.0.1:8080/stop/, обрывает соединение
     elif req.startswith('GET /stop/'):
         return 'close connection'
     else:
         return 'bad request'
 
+#создает нового пользователя и поток для него
 def user(conn, addr):
     while True:
         request = conn.recv(1024).decode()
@@ -106,12 +118,13 @@ def user(conn, addr):
             conn.close()
             break
 
-PORT = 8096
+PORT = 8080
 users = ['alisa']
 sock = socket.socket()
 sock.bind(('', PORT))
 sock.listen()
 
+#отслеживание новых подключений
 while True:
     print("Слушаем порт", PORT)
     conn, addr = sock.accept()
